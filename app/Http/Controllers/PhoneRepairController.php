2d7a3e4437e6c3c50d5c61d515846b56a8e2dcbd
Hassan Mehmood <hassanafridi1913@gmail.com>
2024-03-28 09:39:37 +0500

    feat: added phone repair index page working on phone repair create page

---
+++ app/Http/Controllers/PhoneRepairController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PhoneRepairController extends Controller
+{
+	public function index()
+	{
+		return view("phone-repair.index");
+	}
+
+	public function create()
+	{
+		return view('phone-repair.create');
+	}
+
+
+}
---
+++ resources/views/phone-repair/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.tabler')
+
+@section('content')
+    <h1>Create phone repair</h1>
+@endsection
