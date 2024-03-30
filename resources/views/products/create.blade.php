90b56d2bbfb93610d4ed33a606a85a46c99b49c5
Hassan Mehmood <hassanafridi1913@gmail.com>
2024-03-30 23:47:29 +0500

    feat: made form for add product page

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -62,23 +62,188 @@
                                 <div class="card-body">
                                     <div class="row row-cards">
                                         <div class="col-md-12">
-                                            <x-input name="Phone name" id="name" placeholder="Phone name"
-                                                value="{{ old('name') }}" />
+                                            <x-input name="Name" id="name" value="{{ old('name') }}" />
                                         </div>
 
                                         <div class="col-sm-6 col-md-6">
-                                            <x-input type="number" label="Cost" name="cost" id="cost"
-                                                placeholder="0" value="{{ old('cost') }}" />
+                                            <x-input label="Category" name="category" id="category"
+                                                value="{{ old('category') }}" />
                                         </div>
 
                                         <div class="col-sm-6 col-md-6">
-                                            <x-input type="number" label="Quantity" name="quantity" id="quantity"
-                                                placeholder="0" value="{{ old('quantity') }}" />
+                                            <x-input label="Sub Category" name="sub_category" id="sub_category"
+                                                value="{{ old('sub_category') }}" />
+                                        </div>
+
+                                        <div class="col-md-12 mb-3">
+                                            <label for="description" class="form-label">
+                                                {{ __('Decription') }}
+                                            </label>
+
+                                            <textarea name="description" id="description" rows="5" class="form-control" placeholder="Description...">{{ old('description') }}</textarea>
                                         </div>
 
                                         <div class="col-sm-6 col-md-6">
-                                            <x-input type="number" label="Code" name="code" id="code"
-                                                placeholder="0" value="{{ old('code') }}" />
+                                            <x-input label="Manufacturer" name="manufacturer" id="manufacturer"
+                                                value="{{ old('manufacturer') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Device" name="device" id="device"
+                                                value="{{ old('device') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="SKU" name="sku" id="sku"
+                                                value="{{ old('sku') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="UPC Code" name="upc_code" id="upc_code"
+                                                value="{{ old('upc_code') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6 mb-3">
+                                            <label for="category_id" class="form-label">
+                                                Is Barcode
+                                            </label>
+                                            <select name="repair_part_id" id="repair_part_id"
+                                                class="form-select @error('repair_part_id') is-invalid @enderror">
+                                                <option value="true">Yes</option>
+                                                <option value="false">No</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Valuation method" name="valuation_method" id="valuation_method"
+                                                value="{{ old('valuation_method') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="New Stock Adjustment" name="new_stock_adjustment"
+                                                id="new_stock_adjustment" value="{{ old('new_stock_adjustment') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="New Inventory Item Cost" name="new_inventory_item_cost"
+                                                id="new_inventory_item_cost"
+                                                value="{{ old('new_inventory_item_cost') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Tax Class" name="tax_class" id="tax_class"
+                                                value="{{ old('tax_class') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Tax inclusive" name="tax_inclusive" id="tax_inclusive"
+                                                value="{{ old('tax_inclusive') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Retail price" name="retail_price"
+                                                id="retail_price" placeholder="0" value="{{ old('retail_price') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Cost price" name="cost_price" id="cost_price"
+                                                placeholder="0" value="{{ old('cost_price') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Sale price" name="sale_price" id="sale_price"
+                                                placeholder="0" value="{{ old('sale_price') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Minimum price" name="minimum_price"
+                                                id="minimum_price" placeholder="0" value="{{ old('minimum_price') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="On hand quantity" name="on_hand_quantity"
+                                                id="on_hand_quantity" placeholder="0"
+                                                value="{{ old('on_hand_quantity') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Stock warning" name="stock_warning"
+                                                id="stock_warning" placeholder="0" value="{{ old('stock_warning') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Reorder level" name="reorder_level"
+                                                id="reorder_level" placeholder="0" value="{{ old('reorder_level') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Manage serialized" name="manage_serialized"
+                                                id="manage_serialized" value="{{ old('manage_serialized') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Condition" name="condition" id="condition"
+                                                value="{{ old('condition') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Supplier" name="supplier" id="supplier"
+                                                value="{{ old('supplier') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Physical location" name="physical_location"
+                                                id="physical_location" placeholder="0"
+                                                value="{{ old('physical_location') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Warranty" name="warranty" id="warranty"
+                                                placeholder="0" value="{{ old('warranty') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input label="Warranty time frame" name="warranty_time_frame"
+                                                id="warranty_time_frame" value="{{ old('warranty_time_frame') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="IMEI" name="imei" id="imei"
+                                                placeholder="0" value="{{ old('imei') }}" />
+                                        </div>
+
+
+                                        <div class="col-sm-6 col-md-6 mb-3">
+                                            <label for="display_pos" class="form-label">
+                                                Display on point of sale
+                                            </label>
+                                            <select name="display_pos" id="display_pos" class="form-select">
+                                                <option value="true">Yes</option>
+                                                <option value="false">No</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6 mb-3">
+                                            <label for="display_widget" class="form-label">
+                                                Display on widget
+                                            </label>
+                                            <select name="display_widget" id="display_widget" class="form-select">
+                                                <option value="true">Yes
+                                                </option>
+                                                <option value="false">No</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Commision percentage"
+                                                name="comission_percentage" id="comission_percentage" placeholder="0"
+                                                value="{{ old('comission_percentage') }}" />
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-6">
+                                            <x-input type="number" label="Comission Amount" name="comission_amount"
+                                                id="comission_amount" placeholder="0"
+                                                value="{{ old('comission_amount') }}" />
                                         </div>
                                     </div>
                                 </div>
